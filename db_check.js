6622f0f3947be3bcfd11a0c5ae553c155e4565a3
ilNotchs <juanestebanvalenciapuerta@gmail.com>
2026-04-30 10:08:56 -0500

    Elimincacion de cambios antigravity

--- db_check.js
+++
@@ -1,8 +0,0 @@
-const db = require('./src/config/db');
-
-async function run() {
-    const [rows] = await db.query("SHOW COLUMNS FROM reservas");
-    console.log(rows);
-    process.exit(0);
-}
-run();
